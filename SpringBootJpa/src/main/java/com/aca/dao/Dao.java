2930df4fa15d795c5844716512c5fa7c9303d3d7
AlexDjalovic <adaleksandardjalovic12@gmail.com>
2018-05-15 15:58:43 +0200

    komentar za dao klasu oko izbora CRUDRepository i Repository Interfejsa

--- SpringBootJpa/src/main/java/com/aca/dao/Dao.java
+++ SpringBootJpa/src/main/java/com/aca/dao/Dao.java
@@ -8,4 +8,6 @@ import com.aca.entities.Ticket;
 @Repository
 public interface Dao extends CrudRepository<Ticket, Integer> {
 //kao genreics stavljaju se entitet na koji se odnosi i vrsta id
+	//ako hocu da koristim unapred definisane metode kao sto su save findone,findAll() onda koristim crudrepository interfejs 
+//ako hocu sam da kreiram metode za
 }
